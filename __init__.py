f7e044d11999881c914819e2d5106c95016536cb
Aaron Windsor <aaron.windsor@gmail.com>
2010-08-16 16:19:11 -0700

    Adding BackendCore and BackendSQLite to main __init__.py

--- __init__.py
+++ __init__.py
@@ -6,3 +6,5 @@ Graph = config.dispatch_graph
 Forest = config.dispatch_forest
 
 from yaupon.data_structures import ydeque, ydict, ysorted, yheap, yunion_find
+
+from yaupon.backend import BackendCore, BackendSQLite
