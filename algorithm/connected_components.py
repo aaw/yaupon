0529b1516424540abb21278d069c2ef5947e31d1
Aaron Windsor <aaron.windsor@gmail.com>
2010-11-06 17:25:06 -0400

    added Cheriyan-Mehlhorn/Gabow connected components, but need to merge this with recent origin changes to the traversals before I go any further with it

---
+++ algorithm/connected_components.py
@@ -0,0 +1,53 @@
+from yaupon.data_structures import ydeque, ydict
+from yaupon.traversal import *
+
+# A composite visitor implementing the Cheriyan-Mehlhorn/Gabow
+# strongly connected components algorithm
+class ConnectedComponents(CompositeVisitor):
+    dependencies = [DiscoverTime]
+
+    def __init__(self, agg):
+        CompositeVisitor.__init__(self, agg)
+        self.S = ydeque(backend=agg)
+        self.P = ydeque(backend=agg)
+        self.next_component_id = 0
+
+    def discover_vertex(self, v):
+        self.S.append(v)
+        self.P.append(v)
+
+    def back_edge(self, e):
+        v,w = e
+        if self.value.get(w) is not None:
+            return
+
+        while self.properties.DiscoverTime[self.P[-1]] > \
+              self.properties.DiscoverTime[w]:
+            self.P.pop()
+
+    def finish_vertex(self, v):
+        if v == self.P[-1]:
+            while self.value.get(v) is None:
+                self.value[self.S.pop()] = next_component_id
+            self.P.pop()
+            next_component_id += 1
+
+class StronglyConnectedComponents(object):
+    def __init__(self, graph):
+        self.description = 'Cheriyan-Mehlhorn/Gabow strongly-connected components'
+        generator = depth_first_generator(graph)
+        visitor = AggregateVisitor(backend=graph, 
+                                   visitors=[DiscoverTime, ConnectedComponents]) 
+        #TODO: haven't merged with most recent changes on origin, but when I do, DiscoverTime
+        #      doesn't need to be mentioned above
+        traverse(root_vertices=[graph.vertices().next()],
+                 visitor=visitor,
+                 generator=depth_first_generator(graph))
+        self.components = visitor.ConnectedComponents
+
+    def __call__(self, v):
+        return self.components[v]
+
+def compile(graph):
+    return StronglyConnectedComponents(graph)
+
